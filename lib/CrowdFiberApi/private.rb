aaaa61b5fc3e090ee6ae827c897c0b56ac4971d1
Ellery J. Durgin <ellery@civitium.com>
2017-06-14 22:05:03 -0400

    added travis config and fixed indenting issues

--- lib/CrowdFiberApi/private.rb
+++ lib/CrowdFiberApi/private.rb
@@ -5,15 +5,15 @@ class CrowdFiberApi::Private
   attr_reader :config
   
   def initialize(config = {})
-     if config.nil? || config[:api_key].nil? 
-       raise ArgumentError.new(":api_key is a required argument to initialize Api")
-     end
+    if config.nil? || config[:api_key].nil? 
+      raise ArgumentError.new(":api_key is a required argument to initialize Api")
+    end
      
-     if config.nil? || config[:api_url].nil?
-       raise ArgumentError.new(":api_url is a required argument to initialize CrowdFiberApi")
-     end
-     @config = config
-   end
+    if config.nil? || config[:api_url].nil?
+      raise ArgumentError.new(":api_url is a required argument to initialize CrowdFiberApi")
+    end
+    @config = config
+  end
 
   
 
